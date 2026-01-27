86920cd20e6af8afe990458e9562053449616f83
xuchenhao <48084123+xuchenhao@users.noreply.github.com>
2026-01-27 15:57:36 +0800

    Update block_file_cache.cpp

--- be/src/io/cache/block_file_cache.cpp
+++ be/src/io/cache/block_file_cache.cpp
@@ -169,6 +169,7 @@ BlockFileCache::BlockFileCache(const std::string& cache_base_path,
     _evict_by_size_metrics_matrix[FileCacheType::NORMAL][FileCacheType::TTL] =
             std::make_shared<bvar::Adder<size_t>>(_cache_base_path.c_str(),
                                                   "file_cache_evict_by_size_normal_to_ttl");
+
     _evict_by_size_metrics_matrix[FileCacheType::INDEX][FileCacheType::DISPOSABLE] =
             std::make_shared<bvar::Adder<size_t>>(_cache_base_path.c_str(),
                                                   "file_cache_evict_by_size_index_to_disposable");
