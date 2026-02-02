9cdcb277173f9a9a03c30451ee44311250c01342
xuchenhao <419062425@qq.com>
2026-02-02 08:48:31 +0800

    Fix BE UT core dump.

--- be/src/vec/data_types/serde/data_type_string_serde.h
+++ be/src/vec/data_types/serde/data_type_string_serde.h
@@ -56,7 +56,14 @@ inline void escape_string(const char* src, size_t* len, char escape_char) {
         if (escape_next_char) {
             ++src;
         } else {
-            *dest_ptr++ = *src++;
+            // This conditional check serves two purposes:
+            // 1. When dest_ptr == src, skip meaningless self-assignment
+            // 2. Avoid dereference crash when both pointers are null
+            if (dest_ptr != src) {
+                *dest_ptr = *src;
+            }
+            dest_ptr++;
+            src++;
         }
     }
 
